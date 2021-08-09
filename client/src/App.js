df55c365ea6dcd38d640dd799bd6ceb0be734373
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-08-08 21:38:01 -0400

    react-markdown for turning md into component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react'
+import ReactMarkdown from 'react-markdown'
 
 function App() {
   const [data, setData] = useState({ message: '' })
+  const postId = '602b83f68995a1ff2ac71fcd'
 
   useEffect(() => {
-    fetch(`${process.env.REACT_APP_API}/`, {
+    fetch(`${process.env.REACT_APP_API}/posts/${postId}`, {
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
@@ -12,12 +14,17 @@ function App() {
     })
       .then((res) => res.json())
       .then((res) => {
-        setData({ message: res.message })
+        console.log(res)
+        setData({ message: res.data.content })
       })
       .catch((err) => console.error(err))
   }, [])
 
-  return <div className="App">{data.message}</div>
+  return (
+    <div className="App">
+      <ReactMarkdown children={data.message} />
+    </div>
+  )
 }
 
 export default App
